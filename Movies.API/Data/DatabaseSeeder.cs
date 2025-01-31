d21848a98ae26968d7831e4b9a6c0412d38a36f7
TomaszLenart <t.lenart234@gmail.com>
2025-01-31 12:04:52 +0100

    Movies.API

---
+++ Movies.API/Data/DatabaseSeeder.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.API.Data
+{
+    public static class DatabaseSeeder
+    {
+        public static async Task SeedDatabase(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var moviesContext = scope.ServiceProvider.GetRequiredService<MoviesAPIContext>();
+                await MoviesContextSeed.SeedAsync(moviesContext);
+            }
+        }
+    }
+}
---
+++ Movies.API/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+await DatabaseSeeder.SeedDatabase(app.Services);
+app.Run();
